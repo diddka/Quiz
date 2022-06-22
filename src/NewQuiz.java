b9d45144b2f84e9d1d82476e605e749fa3b6f58e
didka <danailova_rz@abv.bg>
2022-06-22 12:53:08 +0300

    Project Quiz-not working yet

--- src/NewQuiz.java
+++ src/NewQuiz.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class NewQuiz {
     public static String[][] readKategory(String choice) {
@@ -12,21 +14,17 @@ public class NewQuiz {
         } else if (choice.equalsIgnoreCase("funny")) {
             fileLokation = "src\\funnyQuestions.csv";
         }
-        String line = "";
-        String[] value = new String[5];
+        String line;
+        String[] value;
         String[][] file = new String[5][5];
         int rows = 0;
         try {
             BufferedReader br = new BufferedReader(new FileReader(fileLokation));
             while ((line = br.readLine()) != null) {
                 value = line.split(",");
-                for (int i = 0; i < value.length; i++) {
-                    file[rows][i] = value[i];
-                }
+                System.arraycopy(value, 0, file[rows], 0, value.length);
                 rows++;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,25 +37,25 @@ public class NewQuiz {
         chosenCategory = asList.toArray(new String[0][0]);
         String question = "";
         String[] answers = new String[4];
-        int row = 0;
-        int col = 0;
+
+
         for (int i = 0; i < 5; i++) {
             if (i == 0) {
                 question = chosenCategory[0][i];
 
             } else {
                 answers[i - 1] = chosenCategory[0][i];
-                col++;
+
             }
         }
         List<String> answersToList = Arrays.asList(answers);
         Collections.shuffle(answersToList);
 
         StringBuilder askQuestion = new StringBuilder();
-        askQuestion.append(question + "\n");
+        askQuestion.append(question).append("\n");
         int numeric = 1;
-        for (int i = 0; i < answers.length; i++) {
-            askQuestion.append(numeric + "." + answers[i] + "\n");
+        for (String answer : answers) {
+            askQuestion.append(numeric).append(".").append(answer).append("\n");
             numeric++;
         }
         askQuestion.toString();
@@ -73,7 +71,7 @@ public class NewQuiz {
         String choice = sc.nextLine();
         String[][] chosenCategory = readKategory(choice);
         StringBuilder rightAnswers = new StringBuilder();
-        String answer = "";
+        String answer;
         for (int i = 0; i < chosenCategory[0].length; i++) {
             rightAnswers.append(chosenCategory[i][1]);
         }
@@ -82,21 +80,17 @@ public class NewQuiz {
         boolean gameOver = true;
         int score = 0;
         String shuffleQuestion = String.valueOf(shuffleQuestion(chosenCategory));
-        do {
-            System.out.println(shuffleQuestion);
-            answer = sc.nextLine();
-            if (result.toLowerCase().contains(answer.toLowerCase())){
-                score++;
-                System.out.println("You guess right.  Score: " + score + "/5");
-            }else{
-                gameOver = false;
-                System.out.println("Wrong answer. Your score:" + score + "/5");
-            }
-        } while (false || score == 5);
+        System.out.println(shuffleQuestion);
+        answer = sc.nextLine();
+        if (result.toLowerCase().contains(answer.toLowerCase())){
+            score++;
+            System.out.println("You guess right.  Score: " + score + "/5");
+        }else{
+            gameOver = false;
+            System.out.println("Wrong answer. Your score:" + score + "/5");
+        }
         if(!gameOver){
             System.out.println("Game over.\nYour score: " + score + "/5");
-        }else if (score==5) {
-            System.out.println("Game over.\nYour score: 5/5\n YOU WIN");
         }
 
 
